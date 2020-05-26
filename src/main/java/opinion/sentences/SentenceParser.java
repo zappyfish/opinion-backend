3a415552c8520ad016f18c485e25bf8d28bac823
annaouyang <anna.ouyang@gmail.com>
2020-05-25 22:21:12 -0400

    sentence parser attempt

--- src/main/java/opinion/sentences/SentenceParser.java
+++ src/main/java/opinion/sentences/SentenceParser.java
@@ -1,25 +1,36 @@
 package opinion.sentences;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Iterator;
 
 class SentenceParser implements Iterator<String> {
 
-    SentenceParser(String strSentence) {
+    private String[] mWords;
+    private int mWordIndex;
 
+
+    public SentenceParser(String strSentence) {
+        mWords = strSentence.split("\\s+");
+        mWordIndex = 0;
+        for (int i = 0; i < mWords.length; i++) {
+            // TODO(anna): improve this when it breaks
+            mWords[i] = mWords[i].replaceAll("[^\\w]", "");
+        }
     }
 
+    @Nullable
     @Override
     public String next() {
-        return null;
+        if (!hasNext()) {
+            return null;
+        }
+
+        return mWords[mWordIndex++];
     }
 
     @Override
     public boolean hasNext() {
-        return false;
-    }
-
-    @Override
-    public void remove() {
-        return;
+        return mWordIndex < mWords.length;
     }
 }
--- src/test/java/opinion/sentences/SentenceParserTest.java
+++ src/test/java/opinion/sentences/SentenceParserTest.java
@@ -1,13 +1,32 @@
 package opinion.sentences;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class SentenceParserTest {
 
     @Test
-    public void firstTest() {
-        Assert.assertTrue(false);
+    public void testHasNext() {
+        SentenceParser sp = new SentenceParser("hello my name is anna. ");
+        final int numExpectedWords = 5;
+        for (int i = 0; i < numExpectedWords; i++) {
+            Assert.assertTrue(sp.hasNext());
+            sp.next();
+        }
+        Assert.assertFalse(sp.hasNext());
+    }
+
+    @Test
+    public void testNext() {
+        SentenceParser sp = new SentenceParser(" hello . my name. is anna. ");
+        String[] expectedWords = {"hello", "my", "name", "is", "anna"};
+        for (int i = 0; i < expectedWords.length; i++) {
+            Assert.assertEquals(expectedWords[i], sp.next());
+        }
+
+        Assert.assertNull(sp.next());
     }
 
 }
