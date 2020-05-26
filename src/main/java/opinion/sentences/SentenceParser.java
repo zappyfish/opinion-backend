20766547fdad240d89c8328242ed7f5d80120edb
annaouyang <anna.ouyang@gmail.com>
2020-05-25 23:04:50 -0400

    added javadoc comments

--- src/main/java/opinion/sentences/SentenceParser.java
+++ src/main/java/opinion/sentences/SentenceParser.java
@@ -4,32 +4,41 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
 
+/**
+ * Implements Iterator interface to iterate over words in a sentence
+ */
 class SentenceParser implements Iterator<String> {
 
-    private String[] mWords;
-    private int mWordIndex;
+    private String[] mWords; // array of each word in sentence
+    private int mWordIndex; // index of current word in mWords array
 
-
+    /**
+     * Initializes member variables
+     * @param strSentence sentence to be parsed
+     */
     public SentenceParser(String strSentence) {
-        mWords = strSentence.trim().split("\\W+"); //
+        // places all words in sentence into an array by splitting sentence on any non-word character
+        mWords = strSentence.trim().split("\\W+"); // need to trim whitespace first
         mWordIndex = 0;
-        for (int i = 0; i < mWords.length; i++) {
-            // TODO(anna): improve this when it breaks
-            mWords[i] = mWords[i].replaceAll("[^\\w]", "");
-        }
     }
 
     @Nullable
     @Override
+    /**
+     * returns next word in sentence, null if there is no next word
+     */
     public String next() {
         if (!hasNext()) {
             return null;
         }
 
-        return mWords[mWordIndex++];
+        return mWords[mWordIndex++]; // get next word in sentence
     }
 
     @Override
+    /**
+     * returns true if there are still words left in sentence to be processed
+     */
     public boolean hasNext() {
         return mWordIndex < mWords.length;
     }
